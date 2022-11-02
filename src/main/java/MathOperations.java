6dc55794b16185232cbda418ee1be4385326417e
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-02 16:19:58 -0500

    finished lecture

--- src/main/java/MathOperations.java
+++ src/main/java/MathOperations.java
@@ -1,6 +1,7 @@
 import org.apache.commons.math3.util.Precision;
 
 public class MathOperations {
+
     public static long add(long input1, long input2){
         return input1 + input2;
     }
@@ -8,4 +9,14 @@ public class MathOperations {
     public static double tip(double bill, double tip){
         return Precision.round(bill * (tip / 100), 2);
     }
+
+    public static int[] addOneToArray(int[] numbers){
+        int[] newNumbers = new int[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            newNumbers[i] = numbers[i] + 1;
+        }
+        return newNumbers;
+    }
+
+
 }
--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,8 +1,29 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
+    ArrayList<Integer> arrayList = new ArrayList<>();
+ArrayList<Integer> arrayList2 = new ArrayList<>();
+
+    // this runs BEFORE the test:
+    @Before
+    public void setUpValues(){
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList2.add(1);
+        arrayList2.add(2);
+    }
+
+    @Test
+    public void testObjectsSameness(){
+        // this will fail right now
+        assertSame(arrayList, arrayList2);
+    }
 
     @Test
     public void additionMethodTest() {
@@ -17,7 +38,14 @@ public class MyFirstTest {
         double cost = 22.56;
         double tip = 20;
 
-        assertEquals(4.51, MathOperations.tip(cost, tip), 0.05);
+        assertEquals(4.51, MathOperations.tip(cost, tip), 0);
+    }
+    @Test
+    public void testArrayEquality() {
+        int[] startArray = {1, 2, 3, 4, 5};
+        int[] endArray = {2, 3, 4, 5, 6};
+
+        assertArrayEquals(endArray, MathOperations.addOneToArray(startArray));
     }
 
 }
