d477b26f88541f72bda005fc661109d086f4f20a
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-02 11:52:57 -0500

    completed first maven exercise

--- src/main/java/MavenExercise.java
+++ src/main/java/MavenExercise.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
 
 public class MavenExercise {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter any input:");
-    }
+        // scanner for asking the user for input:
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter anything:  ");
+        // taking the user input and outputting it to the console:
+            String userInput = scanner.nextLine();
+            System.out.println("You entered: " + userInput);
+        // if else statement to check if the user input is not a number if it is a number it will tell you it is:
+            if(!StringUtils.isNumeric(userInput)) {
+                System.out.println(userInput + " is not a number!");
+            } else {
+                System.out.println(userInput + " is a number!");
+            }
+        // souts to swap case of string entered, check if its numeric, and reverse the string:
+            System.out.println(StringUtils.swapCase(userInput));
+            System.out.println(StringUtils.isNumeric(userInput));
+            System.out.println(StringUtils.reverse(userInput));
+        }
 }
