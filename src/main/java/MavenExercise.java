3d985875be491e6ca2900df1a078464b2fb912e6
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-02 11:01:51 -0500

    started maven exercises

---
+++ src/main/java/MavenExercise.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any input:");
+    }
+}
